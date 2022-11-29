092bd5e5c74638257cd3d7b096d5d1ea8f1789f6
loky <loky280280@gmail.com>
2022-11-29 17:56:26 +0100

    MouvementEnigme+suppressionCanva
    seulement la prog du mouvement a ete fait .
    Faire contrôles (bouton par recast et paramétrer le drag and drop)

--- Assets/script/script enigme par perso/enigme1/InteractionMachinisteTuto.cs
+++ Assets/script/script enigme par perso/enigme1/InteractionMachinisteTuto.cs
@@ -11,17 +11,81 @@ public class InteractionMachinisteTuto : MonoBehaviour
     public float xTransform;
     public float yTransform;
     public float zTransform;
+    public int position = 10;
 
-    //position_interface => X = 1.370746  / Y = -23.02849     /   Z = 37.24416
+    //position_interface => X = 24.20001/ Y = 6.9     /   Z = -3.9
 
-    //position_interface_Hors_Cam =>  X = 1.370746  / Y = -42.6     /   Z = 71.4
+    //position_interface_Hors_Cam =>  X = 24.20001  / Y = 6.9    /   Z = 36.1
 
-    //Vector => X = 0   /  Y = -19,57151   /   Z = 34,15584
+    //Vector => X = 0   /  Y =0   /   Z = -40
 
+    // cam =>  X= 0   Y=12.1     Z=-10
+    
     public void Button_Interaction_Machiniste1()
 
     {
-        Debug.Log("Machiniste1");
-        panel.transform.Translate(0,19,-34);
+        
+        Debug.Log("test");
+        position = 1;
+        //panel.transform.Translate(24.20001,6.9,-3.9);
+        panel.transform.Translate(24, 7, -4);
+        //panel.transform.Translate(xTransform, yTransform, zTransform);
+    }
+    
+    void Update()
+    {
+        //position de base
+        if (position == 0)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller dans le champ
+        else if (position == 1)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = -40;
+        }
+
+        //position de base
+        else if (position == 0)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller a droite
+        else if (position == 2)
+        {
+            xTransform = 10;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller a gauche
+        else if (position == 3)
+        {
+            xTransform = -10;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller hors champ
+        else if (position == 4)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = 40;
+
+        }
+
     }
 }
---
+++ Assets/script/script enigme par perso/enigme1/buttonAllerADroite.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UIElements;
+
+public class buttonAllerADroite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    InteractionMachinisteTuto interactionMachinisteTuto;
+    public void Button_Interaction_AllerADroite()
+
+    {
+
+        //Debug.Log("Machiniste1");
+        interactionMachinisteTuto.position = 2;
+       
+    }
+}
