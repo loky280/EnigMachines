f505f7c3a1a835c89a3c90955ddbee0389e84c1b
loky280 <loky280280@gmail.com>
2022-11-30 12:00:02 +0100

    résolution problème raycast
    Unification des scripts d'interaction, mais problème avec le translate par des vecteur3

--- Assets/script/Interaction.cs
+++ Assets/script/Interaction.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Interaction : MonoBehaviour {
 
-    InteractionMachinisteTuto interactionMachinisteTuto;
-    public void selectionner() {
+    // Variables
+    [SerializeField]
+    GameObject panel;
 
-        //Debug.Log("test");
+    public float xTransform;
+    public float yTransform;
+    public float zTransform;
+    public float position = 0;
+    //var entrerDansLeChamp = Vector3.Dot(rigidbody.velocity, transform.forward);
+    //InteractionMachinisteTuto interactionMachinisteTuto;
+    //public void selectionner() {
 
-    }
+    //Debug.Log("test");
+
+    //}
     public void Update(){
         
         RaycastHit hit;
@@ -17,13 +28,60 @@ public class Interaction : MonoBehaviour {
         if(Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition),out hit)) {
             if (hit.transform.tag == "machiniste1") {
                 Debug.Log("machiniste1");
-                interactionMachinisteTuto.position = 1;
+                position = 1;
 
             }
 
 
         }
 
+        //panel.transform.SetPositionAndRotation(xTransform, yTransform, zTransform);
+        panel.transform.Translate(xTransform, yTransform, zTransform);
 
+
+        //position de base
+        if (position == 0)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller dans le champ
+        else if (position == 1)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = -40;
+        }
+
+
+        //aller a droite
+        else if (position == 2)
+        {
+            xTransform = 10;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller a gauche
+        else if (position == 3)
+        {
+            xTransform = -10;
+            yTransform = 0;
+            zTransform = 0;
+
+        }
+
+        //aller hors champ
+        else if (position == 4)
+        {
+            xTransform = 0;
+            yTransform = 0;
+            zTransform = 40;
+
+        } 
     }
 }
---
+++ Assets/script/script enigme par perso/enigmeTuto/InteractionMachinisteTuto.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionMachinisteTuto : MonoBehaviour
+{
+    // Variables
+    [SerializeField]
+    GameObject panel;
+
+
+    public float xTransform;
+    public float yTransform;
+    public float zTransform;
+    public int position;
+
+    //position_interface => X = 24.20001/ Y = 6.9     /   Z = -3.9
+
+    //position_interface_Hors_Cam =>  X = 24.20001  / Y = 6.9    /   Z = 36.1
+
+    //var entrerDansLeChamp = Vector3.Dot(rigidbody.velocity, transform.forward);
+
+    //Vector => X = 0   /  Y =0   /   Z = -40
+
+    // cam =>  X= 0   Y=12.1     Z=-10
+    /*
+      public void Button_Interaction_Machiniste1()
+
+      {
+
+          Debug.Log("test");
+          position = 1;
+          //panel.transform.Translate(24.20001,6.9,-3.9);
+          //panel.transform.Translate(24, 7, -4);
+          panel.transform.Translate(xTransform, yTransform, zTransform);
+      }
+
+      void Update()
+      {
+
+          //panel.transform.Translate(xTransform, yTransform, zTransform);
+
+          //position de base
+          if (position == 0)
+          {
+              xTransform = 0;
+              yTransform = 0;
+              zTransform = 0;
+
+          }
+
+          //aller dans le champ
+          else if (position == 1)
+          {
+              xTransform = 0;
+              yTransform = 0;
+              zTransform = -40;
+          }
+
+
+          //aller a droite
+          else if (position == 2)
+          {
+              xTransform = 10;
+              yTransform = 0;
+              zTransform = 0;
+
+          }
+
+          //aller a gauche
+          else if (position == 3)
+          {
+              xTransform = -10;
+              yTransform = 0;
+              zTransform = 0;
+
+          }
+
+          //aller hors champ
+          else if (position == 4)
+          {
+              xTransform = 0;
+              yTransform = 0;
+              zTransform = 40;
+
+          }
+
+      }*/
+}
