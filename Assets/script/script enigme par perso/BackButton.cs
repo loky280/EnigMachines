dfa30217f679770f9ff2df51e353aa905156aafc
loky280 <loky280280@gmail.com>
2022-12-01 13:15:30 +0100

    résolution du canvas apparitionde l'enigme reussite
    faire l'énigme avec drag and drop

--- Assets/script/script enigme par perso/BackButton.cs
+++ Assets/script/script enigme par perso/BackButton.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class BackButton : MonoBehaviour
 {
 
     [SerializeField]
     GameObject panel;
-    public void backButton()
+
+    [SerializeField]
+    GameObject backBouton;
+
+/*
+    void Start()
     {
+        Button btn = boutonback.GetComponent<Button>();____________creer un input de bouton
+        btn.onClick.AddListener(backButton);
+    }*/
+     public void backButton()
+    {
+
+        Debug.Log("back");
+
         panel.SetActive(false);
-
-
+        backBouton.SetActive(false);
 
     }
 }
