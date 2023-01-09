870d246b137970b153afe6f3b0c83838b74a03b1
loky280 <loky280280@gmail.com>
2023-01-09 11:14:55 +0100

    polich +correction faute

---
+++ Assets/script/script enigme par perso/enigmeTuto/son.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Media;
+using UnityEngine;
+
+
+public class son : MonoBehaviour
+{
+    public AudioSource sonAmbiance;
+    public bool SonAmbianceActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
